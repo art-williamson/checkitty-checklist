70ff4fe487da4c782dc40a1f53ab9cf70e51d0a1
Art Williamson <alwilliamson@xactware.com>
2019-03-12 07:02:03 -0600

    added tableview, prototype cell and made checkmark appear and disappear on click

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -15,16 +15,43 @@ class CheckListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: overrides
+    override func tableView(_ tableView: UITableView, didSelectRowAt index: IndexPath) {
+        //set the checkmark on or off for the cell
+        if let cell = tableView.cellForRow(at: index) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+
+        tableView.deselectRow(at: index, animated: true)
+    }
+
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
 
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem",
                                                      for: indexPath)
-            return cell
+        let label = cell.viewWithTag(1000) as! UILabel
+
+        if indexPath.row % 5 == 0 {
+            label.text = "Something nonsensical"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Something whimsical"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Something tragic"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Something wise"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Yo mama"
+        }
+        return cell
     }
 }
 
