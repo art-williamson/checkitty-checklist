5a7a15e4ab5925fbf600223a9924f3e6c08369a0
Art Williamson <alwilliamson@xactware.com>
2019-03-20 09:13:40 -0600

    code cleanup. added detail text to the lists to display the number of remaining itemsin the checklist. copied files back over to desktop due to a weird save in a different folder.

---
+++ Checklists/Checklists/Data Models/DataModel.swift
@@ -0,0 +1,81 @@
+//
+//  DataModel.swift
+//  Checklists
+//
+//  Created by Art Williamson on 3/19/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+import Foundation
+
+class DataModel {
+  var lists = [Checklist]()
+  var indexOfSelectedChecklist: Int {
+    get {
+      return UserDefaults.standard.integer(forKey: "ChecklistIndex")
+    }
+    set {
+      UserDefaults.standard.set(newValue, forKey: "ChecklistIndex")
+      UserDefaults.standard.synchronize()
+    }
+  }
+
+  init() {
+    loadChecklists()
+    registerDefaults()
+    handleFirstTime()
+  }
+  
+  //MARK: Save and load data
+  func documentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+
+  func dataFilePath() -> URL {
+    return documentsDirectory().appendingPathComponent("Checklists.plist")
+  }
+
+  func saveChecklists() {
+    let encoder = PropertyListEncoder()
+    do {
+      // You encode lists instead of "items"
+      let data = try encoder.encode(lists)
+      try data.write(to: dataFilePath(),
+                     options: Data.WritingOptions.atomic)
+      print("Error encoding item array!")
+    } catch { }
+  }
+
+  func loadChecklists() {
+    let path = dataFilePath()
+    if let data = try? Data(contentsOf: path) {
+      let decoder = PropertyListDecoder()
+      do {
+        // You decode to an object of [Checklist] type to lists
+        lists = try decoder.decode([Checklist].self, from: data)
+      } catch {
+        print("Error decoding item array!")
+      }
+    }
+  }
+
+  func registerDefaults() {
+    let dictionary: [String:Any] = ["ChecklistIndex": -1, "FirstTime": true]
+    UserDefaults.standard.register(defaults: dictionary)
+  }
+
+  func handleFirstTime() {
+    let userDefaults = UserDefaults.standard
+    let firstTime = userDefaults.bool(forKey: "FirstTime")
+
+    if firstTime {
+      let checklist = Checklist(name: "List")
+      lists.append(checklist)
+
+      indexOfSelectedChecklist = 0
+      userDefaults.set(false, forKey: "FirstTime")
+      userDefaults.synchronize()
+    }
+  }
+}
